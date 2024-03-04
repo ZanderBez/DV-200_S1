56199d136745e1cdddc77c47ad28c1694218a365
ZanderBez <221205@virtualwindow.co.za>
2024-03-04 14:46:39 +0200

    Update Dashboard

--- myapp/src/pages/Dashboard.js
+++ myapp/src/pages/Dashboard.js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from 'react';
 import './Dashboard.css';
-import Dropdown from 'react-bootstrap/Dropdown';
 
 const Dashboard = () => {
   return (
-    <div className='title'>
-      <h1>This is the Dashboard page</h1>
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    <div>
+      <div className='title'>
+        <h1>This is the Dashboard</h1>
+      </div>
+      <div className='row'>
+        <div className='col'>
+          <div className='weather-name'>
+            <h2>New York</h2>
+            <div className='block1'></div>
+          </div>
+        </div>
+        <div className='col'>
+          <div className='weather-graph'>
+          <h2>Graph</h2>
+            <div className='block2'>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
+
