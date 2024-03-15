36ce6e9845a45029b311f46d853b8c8eaed9b81d
ZanderBez <221205@virtualwindow.co.za>
2024-03-15 21:22:50 +0200

    Update grid

--- myapp/src/pages/Dashboard.js
+++ myapp/src/pages/Dashboard.js
@@ -15,24 +15,14 @@ const Dashboard = () => {
         <h3>Discover the latest weather forecasts at Our Website. With our user-friendly interface and reliable API, you'll always know what to expect, rain or shine. Plan your day with confidence  visit us now</h3>
       </div>
       </div>
-      <div className='row'>
-        <div className='col1'>
-          <div className='weather-name'>
-            <h2>New York</h2>
-            <div className='block1'></div>
-          </div>
-        </div>
-        <div className='col1'>
-          <div className='weather-graph'>
-          <h2>Graph</h2>
-            <div className='block2'>
-            <div className='LineChart'>
-       <LineChart />
-       </div>
-            </div>
-          </div>
-        </div>
+      <div className='info-container'>
+      <div className="parent">
+        <div className="div1"></div>
+        <div className="div2"></div>
+        <div className="div3"></div>
       </div>
+      </div>
+      
       </div>
     </div>
   );
